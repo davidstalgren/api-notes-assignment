a0e82f35a3b3bafc24bf307a34065e99f5114af5
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-04-11 09:01:33 +0200

    add backend for the get of printNotes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  
+  connection.connect((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      connection.query('SELECT * FROM notes', (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+          res.json(data);
+        }
+      })
+    }
+  })
 });
 
 router.post('/', (req, res) => {
