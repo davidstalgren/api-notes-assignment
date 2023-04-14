ac2ebde71eaa41f11914af07e6f6c21aa5866fd4
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-04-15 00:18:00 +0200

    add backend route for the edit function

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -62,4 +62,31 @@ router.post('/', (req, res) => {
   })
 })
 
+router.post('/update/:id', (req, res) => {
+  let editedNote = req.body;
+  console.log('edited', editedNote);
+
+  connection.connect((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+
+      let sql = `UPDATE notes SET 
+      noteHeading = '${editedNote.editedNoteHeading}',
+      noteDescription = '${editedNote.editedNoteDescription}',
+      noteContent = '${editedNote.editedNoteContent}'
+      WHERE noteId = ${req.params.id}`;
+
+      connection.query(sql, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('saved new Note', data);
+          res.json(data);
+        }
+      })
+    }
+  })
+})
+
 module.exports = router;
